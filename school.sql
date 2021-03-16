7676400a25a94ff18f23c946297f71372c714ceb
Yandi Rohman <yandy.rhm@gmail.com>
2021-03-16 12:32:55 +0700

    16 mar

--- school.sql
+++ school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Feb 2021 pada 03.36
+-- Waktu pembuatan: 16 Mar 2021 pada 06.31
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.8
 
@@ -76,11 +76,21 @@ CREATE TABLE `achievment_members` (
 
 CREATE TABLE `class` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `major_id` int(11) NOT NULL,
+  `major_id` int(11) DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `grade` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `class`
+--
+
+INSERT INTO `class` (`id`, `major_id`, `name`, `grade`, `created_at`, `updated_at`) VALUES
+(3, 2, 'X RPL 1', 10, '2021-03-10 22:06:48', '2021-03-10 22:06:48'),
+(4, 2, 'XI RPL 1', 11, '2021-03-10 22:06:59', '2021-03-10 22:06:59'),
+(6, 2, 'XII RPL 1', 12, '2021-03-10 22:13:21', '2021-03-10 22:13:21');
 
 -- --------------------------------------------------------
 
@@ -111,6 +121,16 @@ CREATE TABLE `event` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `event`
+--
+
+INSERT INTO `event` (`id`, `title`, `text`, `is_active`, `created_at`, `updated_at`) VALUES
+(4, 'Festival Musim Panas', '<p>yahahahah hayuuuk</p>\r\n', 1, '2021-02-27 20:53:07', '2021-02-27 20:53:07'),
+(5, 'Jadwal Kesenian', '<p>dsfgsdfdfg</p>\r\n', 1, '2021-02-27 20:53:26', '2021-02-27 20:53:26'),
+(6, 'Jadwal UTS', '<p>kjhkjh <strong>kjhjkhbj</strong> <em>jhbjbjh</em></p>\r\n', 1, '2021-03-05 22:00:38', '2021-03-05 22:00:38'),
+(7, 'Jadwal UTS', '<p>jhjhbjh <strong>hvjhvjh</strong> <em>jhjh <strong><s>jhkjhbjhbjh</s></strong></em></p>\r\n', 1, '2021-03-05 22:03:10', '2021-03-05 22:03:10');
 
 -- --------------------------------------------------------
 
@@ -143,6 +163,14 @@ CREATE TABLE `face` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `face`
+--
+
+INSERT INTO `face` (`id`, `photo`, `title`, `text`, `created_at`, `updated_at`) VALUES
+(10, '1615382642.jpg', 'Halo Alam Dunia', 'Tak tahu sampai kapan\r\nJalan yang ku tempuh ini sampai di ujung\r\nLelah... Aku merasa lelah\r\nDengan jalan yang aku tapaki\r\nMungkin memang harus ku kemudikan dengan baik\r\nAgar sampai di tujuan sesuai keinginan\r\nTapi, bisa kah diri ku?\r\nBisakah kemudi itu berkolaborasi dengan pikiran ku ini?\r\nAtaukah kemudi itu yang bisa membawa ku ke jalan yang benar?\r\nTuhan... Ada kah seseorang yang Kau siapkan untuk ku\r\nUntuk bersama menopang beban yang ku pikul ini\r\nAgar mau ku bagi kesedihan ku\r\nMau ku bagi derita ku', '2021-03-07 05:46:20', '2021-03-10 07:24:02'),
+(11, '1615117612.jpg', 'Hmm Aku Suka', 'Lelah... Aku merasa lelah\r\nDengan jalan yang aku tapaki\r\nMungkin memang harus ku kemudikan dengan baik\r\nAgar sampai di tujuan sesuai keinginan', '2021-03-07 05:46:52', '2021-03-07 05:46:52');
 
 -- --------------------------------------------------------
 
@@ -197,10 +225,17 @@ CREATE TABLE `major` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `text` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `logo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `major`
+--
+
+INSERT INTO `major` (`id`, `name`, `text`, `created_at`, `updated_at`) VALUES
+(2, 'Rekayasa Perangkat Lunak', 'kdhfbkjshd askjdfbajsd skjdhfkjasd ksjdhfbkajsdf aksjdhfbkjasdf askjdhfbkjsadf skdjfbkajsdhf askdjhfbkasjhdfb', '2021-03-10 21:46:50', '2021-03-10 21:46:50'),
+(3, 'Akuntansi', 'hbfuksd kshdbfk ksjdhbsj dsuhdbs dkj suhdcb lkjsadhf ljshad sadjfbas lijbflis lsijdbf sadjsbdf slidjfs dfsijdfsad fsdf sadf sad fasdf sadfs fdasdfasdf asdfasdf asdf asdfasdf asdf asdf asdf sadf asdf sadf', '2021-03-10 21:47:14', '2021-03-10 21:47:14');
 
 -- --------------------------------------------------------
 
@@ -219,7 +254,10 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2021_02_28_001359_create_all', 1);
+(1, '2021_02_28_001359_create_all', 1),
+(3, '2021_03_08_121337_add_column_in_profile', 2),
+(4, '2021_03_11_011158_delete_column_in_staff', 3),
+(5, '2021_03_11_011408_delete_column_in_student', 4);
 
 -- --------------------------------------------------------
 
@@ -258,6 +296,7 @@ CREATE TABLE `photo` (
 CREATE TABLE `profile` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `logo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `wa` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -267,9 +306,17 @@ CREATE TABLE `profile` (
   `youtube` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `instagram` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `twitter` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `profile`
+--
+
+INSERT INTO `profile` (`id`, `logo`, `nama`, `address`, `phone`, `wa`, `email`, `schedule`, `facebook`, `youtube`, `instagram`, `twitter`, `text`, `created_at`, `updated_at`) VALUES
+(1, 'sdfsdf', 'SMK Nurul Islamss', 'sdfsd', 'sdfsdf', 'sdfsdf', 'sdfsd', 'sdfsd', 'sdfsd', 'sddf', 'sdfsdf', 'sdfds', 'sdfdsf', '2021-03-09 16:35:54', '2021-03-09 16:35:54');
 
 -- --------------------------------------------------------
 
@@ -279,7 +326,6 @@ CREATE TABLE `profile` (
 
 CREATE TABLE `staff` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` int(11) NOT NULL,
   `teacher_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `position` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -307,7 +353,6 @@ CREATE TABLE `staff` (
 
 CREATE TABLE `student` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `user_id` int(11) NOT NULL,
   `class_id` int(11) NOT NULL,
   `student_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `national_student_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -359,6 +404,16 @@ CREATE TABLE `vm` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `vm`
+--
+
+INSERT INTO `vm` (`id`, `type`, `value`, `created_at`, `updated_at`) VALUES
+(4, 'misi', 'Misi Satu', '2021-03-09 17:09:00', '2021-03-09 17:09:00'),
+(5, 'misi', 'Misi Dua', '2021-03-09 17:09:32', '2021-03-09 17:09:32'),
+(6, 'misi', 'Misi Tiga', '2021-03-09 17:09:37', '2021-03-09 17:09:37'),
+(8, 'visi', 'Ini visi kami', '2021-03-09 17:10:03', '2021-03-09 17:10:03');
 
 --
 -- Indexes for dumped tables
@@ -468,15 +523,13 @@ ALTER TABLE `profile`
 -- Indeks untuk tabel `staff`
 --
 ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `staff_user_id_index` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `student`
 --
 ALTER TABLE `student`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `student_user_id_index` (`user_id`),
   ADD KEY `student_class_id_index` (`class_id`);
 
 --
@@ -517,7 +570,7 @@ ALTER TABLE `achievment_members`
 -- AUTO_INCREMENT untuk tabel `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `download`
@@ -529,7 +582,7 @@ ALTER TABLE `download`
 -- AUTO_INCREMENT untuk tabel `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `extra`
@@ -541,7 +594,7 @@ ALTER TABLE `extra`
 -- AUTO_INCREMENT untuk tabel `face`
 --
 ALTER TABLE `face`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT untuk tabel `facility`
@@ -565,13 +618,13 @@ ALTER TABLE `gallery`
 -- AUTO_INCREMENT untuk tabel `major`
 --
 ALTER TABLE `major`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `news`
@@ -589,13 +642,13 @@ ALTER TABLE `photo`
 -- AUTO_INCREMENT untuk tabel `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT untuk tabel `student`
@@ -613,7 +666,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `vm`
 --
 ALTER TABLE `vm`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
